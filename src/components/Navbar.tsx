e6b56f15918eb2f1cfd19da29bdfd4e5a2952b1d
mayconblopes <lopes.maycon@gmail.com>
2023-08-26 07:15:59 -0300

    feat: background blur on menu open

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ import CharmMenuHamburger from '../assets/CharmMenuHamburger'
 import logo from '../assets/logo.png'
 import * as styles from './navbar.module.css'
 import { Link } from 'gatsby'
+import { IconButton } from '@mui/material'
 
 export default function Navbar() {
-
   const [menuOpen, setMenuOpen] = useState(false)
 
   function handleMenuClick() {
@@ -15,19 +15,31 @@ export default function Navbar() {
 
   return (
     <Fragment>
-      <div className={styles.menu} style={{ display: menuOpen ? 'flex' : 'none' }}>
-        <Link to='/'>Home</Link>
-        <Link to='/'>Galeria de fotos</Link>
-        <Link to='/'>Documentos</Link>
-        <Link to='/'>Aniversariantes</Link>
-        <Link to='/'>Sobre</Link>
+      <div
+        className={styles.blur}
+        style={{ display: menuOpen ? 'flex' : 'none' }}
+        onClick={handleMenuClick}
+      >
+        <div
+          className={styles.menu}
+          style={{ display: menuOpen ? 'flex' : 'none' }}
+        >
+          <Link to='/'>Home</Link>
+          <Link to='/'>Galeria de fotos</Link>
+          <Link to='/'>Documentos</Link>
+          <Link to='/'>Aniversariantes</Link>
+          <Link to='/'>Sobre</Link>
           <img className={styles.logo} src={logo} alt='logo da IBPG' />
+        </div>
       </div>
-    <nav className={styles.navbar}>
-      <CharmMenuHamburger className={styles.menuIcon} onClick={handleMenuClick}/>
-      <h1 className={styles.title}>Igreja Batista em Parque Guarus</h1>
-      <img className={styles.logo} src={logo} alt='logo da IBPG' />
-    </nav>
+
+      <nav className={styles.navbar}>
+        <IconButton onClick={handleMenuClick} disableTouchRipple>
+          <CharmMenuHamburger className={styles.menuIcon} />
+        </IconButton>
+        <h1 className={styles.title}>Igreja Batista em Parque Guarus</h1>
+        <img className={styles.logo} src={logo} alt='logo da IBPG' />
+      </nav>
     </Fragment>
   )
 }
