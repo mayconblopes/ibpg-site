f7484ed161e0fe1e7e2bea789beecd0c5aa2a6b3
mayconblopes <lopes.maycon@gmail.com>
2023-09-02 16:45:03 -0300

    feature: new component (about page)

---
+++ src/pages/sobre.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import { graphql } from 'gatsby'
+import { display } from '@mui/system'
+
+export default function Sobre({ data }: any) {
+  const sobre = data.allMarkdownRemark.nodes
+
+  return (
+    <Layout>
+      <div
+        style={{
+          padding: '0 15%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '20px',
+        }}
+      >
+        <h1 style={{ fontWeight: 'bold', alignSelf: 'center', fontSize: '14px' }}>
+          Sobre a IBPG
+        </h1>
+        <div className='beautifulText' dangerouslySetInnerHTML={{ __html: sobre[0].html }}></div>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query SobreQuery {
+    allMarkdownRemark(filter: { frontmatter: { type: { eq: "sobre" } } }) {
+      nodes {
+        frontmatter {
+          updated
+          title
+        }
+        html
+      }
+    }
+  }
+`
