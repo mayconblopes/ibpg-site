59fd2e76cd28d119741b07cb04fb2f708c9993aa
mayconblopes <lopes.maycon@gmail.com>
2023-09-03 18:10:10 -0300

    feat: create aniversariantes.tsx

---
+++ src/pages/aniversariantes.tsx
@@ -0,0 +1,50 @@
+import { graphql } from 'gatsby'
+import React, { Fragment } from 'react'
+import Layout from '../components/Layout'
+
+export default function Aniversariantes({ data }: any) {
+  console.log('aniversariantes', data)
+
+  return (
+    <Layout>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          marginTop: '20px',
+          marginRight: 'auto',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            width: '85%',
+          }}
+        >
+          <h1 style={{ textAlign: 'center', marginBottom: '15px' }}>
+            ANIVERSARIANTES DO MÊS
+          </h1>
+          <div>
+            <em>
+              Desejamos a todos os nossos irmãos e irmãs que fazem aniversário
+              neste mês, uma vida de bençãos e muitas felicidades!
+            </em>
+            <div
+              style={{ marginTop: '15px' }}
+              dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+            />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query AniversariantesQuery {
+    markdownRemark(frontmatter: { type: { eq: "aniversariantes" } }) {
+      html
+    }
+  }
+`
