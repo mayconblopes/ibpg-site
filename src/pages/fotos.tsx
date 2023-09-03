72054acea07b64934c0e861eda18be27f17d14ce
mayconblopes <lopes.maycon@gmail.com>
2023-09-03 18:10:49 -0300

    delete 1 file and update 1 file

--- src/pages/fotos.tsx
+++ src/pages/fotos.tsx
@@ -18,7 +18,7 @@ export default function Fotos({ data }: any) {
   console.log(fotos[0].childrenImageSharp[0].fluid)
 
   const [open, setOpen] = useState(false)
-  const [img, setImg] = useState({} as FluidObject | FluidObject[]) 
+  const [img, setImg] = useState({} as FluidObject)
 
   const modalStyle = {
     position: 'absolute' as 'absolute',
@@ -35,20 +35,102 @@ export default function Fotos({ data }: any) {
     setImg(fluid)
   }
 
+  function handleNextImage() {
+    const actual = fotos.filter(
+      (foto: any) => foto.childrenImageSharp[0].fluid.base64 === img.base64
+    )[0]
+    const next = fotos[fotos.indexOf(actual) + 1]
+    console.log(fotos.indexOf(actual))
+    console.log(fotos.indexOf(next))
+
+    if (fotos.indexOf(next) >= 0) {
+      setImg(next.childrenImageSharp[0].fluid)
+    }
+  }
+
+  function handlePrevImage() {
+    const actual = fotos.filter(
+      (foto: any) => foto.childrenImageSharp[0].fluid.base64 === img.base64
+    )[0]
+    const prev = fotos[fotos.indexOf(actual) - 1]
+    console.log(fotos.indexOf(actual))
+    console.log(fotos.indexOf(prev))
+
+    if (fotos.indexOf(prev) >= 0) {
+      setImg(prev.childrenImageSharp[0].fluid)
+    }
+  }
 
   return (
     <Layout>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-        onClick={handleOpenModal}
-      >
+      <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle}>
-
-          <Img fluid={img} />
+          <div
+            style={{
+              backgroundColor: '#0299D4',
+              border: '1px solid black',
+              borderBottom: 'none',
+            }}
+          >
+            <p
+              onClick={handleOpenModal}
+              style={{
+                textAlign: 'right',
+                cursor: 'pointer',
+                marginRight: '10px',
+                fontWeight: 'bold',
+              }}
+            >
+              X
+            </p>
+          </div>
+          <div
+            onClick={handleNextImage}
+            style={{
+              cursor: 'pointer',
+              border: '1px solid black',
+              borderTop: 'none',
+              borderBottom: 'none',
+            }}
+          >
+            <Img fluid={img} />
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              backgroundColor: '#0299D4',
+              padding: '10px',
+              border: '1px solid black',
+              borderTop: 'none',
+            }}
+          >
+            <p
+              style={{
+                cursor: 'pointer',
+                fontSize: '20px',
+                fontWeight: 'bold',
+              }}
+              onClick={handlePrevImage}
+            >
+              {'<'}
+            </p>
+            <p>{'|'}</p>
+            <p
+              style={{
+                cursor: 'pointer',
+                fontSize: '20px',
+                fontWeight: 'bold',
+              }}
+              onClick={handleNextImage}
+            >
+              {'>'}
+            </p>
+          </div>
         </div>
       </Modal>
-      <h1 style={{ textAlign: 'center' }}>Galeria de fotos</h1>
+      <h1 style={{ textAlign: 'center', marginTop: '20px' }}>GALERIA DE FOTOS</h1>
       <div
         style={{
           display: 'flex',
@@ -62,17 +144,18 @@ export default function Fotos({ data }: any) {
           height: '100%',
           margin: 'auto',
           borderRadius: '10px',
-          padding: '15px 0',
+          padding: '15px',
           overflow: 'scroll',
+          boxSizing: 'border-box',
         }}
       >
         {fotos.map((foto, index) => (
           <div
             key={index}
-            style={{ width: '45%', height: 'auto' }}
+            style={{ width: '45%', cursor: 'pointer' }}
             onClick={() => handleOpenModal(foto.childrenImageSharp[0].fluid)}
           >
-            <Img fluid={foto.childrenImageSharp[0].fluid} />
+            <Img fluid={foto.childrenImageSharp[0].fluid} imgStyle={{ borderRadius: '20px', maxHeight: '200px' }}/>
           </div>
         ))}
       </div>
