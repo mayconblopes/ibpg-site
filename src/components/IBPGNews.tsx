1710f3ec7b0a301a30cd4eeb300b5b7f040bac24
mayconblopes <lopes.maycon@gmail.com>
2023-09-09 07:43:29 -0300

    feat: auto IBPG News from YouTube channel

--- src/components/IBPGNews.tsx
+++ src/components/IBPGNews.tsx
@@ -1,11 +1,36 @@
-import { maxWidth } from '@mui/system'
-import React, { Fragment } from 'react'
+import React, { useEffect, useState } from 'react'
 
-type IBPGNewsProps = {
-  videoURL: string
-}
+export default function IBPGNews() {
 
-export default function IBPGNews({ videoURL }: IBPGNewsProps) {
+    // channel_id = UCbMDaos_DocqABRDVKcW23Q
+    // playlist id = PLyUOWLaRVGTUoTX5xjOeYLwut0iBaJ_Ix
+    const channelURL = encodeURIComponent(
+      'https://www.youtube.com/feeds/videos.xml?playlist_id=PLyUOWLaRVGTUoTX5xjOeYLwut0iBaJ_Ix'
+    )
+    const reqURL = `https://api.rss2json.com/v1/api.json?rss_url=${channelURL}`
+  
+    const [ibpgNews, setIbpgNews] = useState(
+      {
+        link: '',
+      },
+    )
+  
+    useEffect(() => {
+      fetch(reqURL)
+        .then(response => response.json())
+        .then(result => {
+          // console.log('RESULT IBPGNEWS--->', result)
+          setIbpgNews(result.items[0])
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }, [])
+  
+    useEffect(() => {
+      console.log(ibpgNews)
+    }, [ibpgNews])
+  
   return (
     <div
       style={{
@@ -30,14 +55,13 @@ export default function IBPGNews({ videoURL }: IBPGNewsProps) {
       >
         <iframe
           height='100%'
-          src={videoURL}
+          src={`https://www.youtube.com/embed/${ibpgNews.link.split('v=')[1]}`}
           title='YouTube video player'
           frameBorder='0'
           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
           allowFullScreen
           style={{
             borderRadius: '10px',
-            // border: '1px solid black',
             boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
             maxWidth: '100%',
           }}
