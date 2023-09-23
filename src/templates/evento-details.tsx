05dc4fdfce2ef041471c641c1ab58b1ab3ab8b1a
mayconblopes <lopes.maycon@gmail.com>
2023-09-23 06:42:36 -0300

    update evento-details.tsx with SEO

--- src/templates/evento-details.tsx
+++ src/templates/evento-details.tsx
@@ -3,6 +3,7 @@ import Layout from '../components/Layout'
 import * as styles from './details.module.css'
 import Img from 'gatsby-image'
 import { Link, graphql } from 'gatsby'
+import { Helmet } from 'react-helmet'
 
 export default function EventoDetails({ data }: any) {
   const coverName = data.markdownRemark.frontmatter.eventCover
@@ -15,6 +16,10 @@ export default function EventoDetails({ data }: any) {
 
   return (
     <Layout>
+      <Helmet>
+       <title>{data.markdownRemark.frontmatter.eventTitle}</title>
+        <meta name='description' content={data.markdownRemark.html.replace(/<[^>]*>/g, '').slice(0, 150)} />
+      </Helmet>
       <div className={styles.container}>
         <h1 className={styles.title}>{data.markdownRemark.frontmatter.eventTitle}</h1>
         <div className={styles.cover}>
