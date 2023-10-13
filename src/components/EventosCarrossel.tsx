4e8e52b056e26df3ab92638e0918750b9ea18669
mayconblopes <lopes.maycon@gmail.com>
2023-10-13 12:29:42 -0300

    update EventosCarrossel.tsx

--- src/components/EventosCarrossel.tsx
+++ src/components/EventosCarrossel.tsx
@@ -16,167 +16,173 @@ export type EventoProps = {
 }
 
 export default function EventosCarrossel() {
-  const [openModal, setOpenModal] = useState(false)
-  const [modalEventTitle, setModalEventTitle] = useState('')
-  const [modalEventDate, setModalEventDate] = useState('')
-  const [modalEventCover, setModalEventCover] = useState('')
-  const [modalEventHTML, setModalEventHTML] = useState('')
-  const [eventNameFromURL, setEventNameFromURL] = useState(
-    window.location.href.split('?').pop() || ''
-  )
+  const isBrowser = typeof window !== 'undefined'
 
-  const context = useContext(PantryContext)
+  if (isBrowser) {
+    const [openModal, setOpenModal] = useState(false)
+    const [modalEventTitle, setModalEventTitle] = useState('')
+    const [modalEventDate, setModalEventDate] = useState('')
+    const [modalEventCover, setModalEventCover] = useState('')
+    const [modalEventHTML, setModalEventHTML] = useState('')
+    const [eventNameFromURL, setEventNameFromURL] = useState(
+      window.location.href.split('?').pop() || ''
+    )
 
+    const context = useContext(PantryContext)
 
-  useEffect(() => {
-    setEventNameFromURL(window.location.href.split('?').pop() || '')
-    // console.log(context)
-    console.log('EVENTNAMEFROMURL', eventNameFromURL)
+    useEffect(() => {
+      setEventNameFromURL(window.location.href.split('?').pop() || '')
+      // console.log(context)
+      console.log('EVENTNAMEFROMURL', eventNameFromURL)
 
-    const eventFromURL = context.eventosFromPantry.filter(
-      evento =>
-        evento.eventTitle.toLocaleLowerCase().replace(' ', '-') ===
-        eventNameFromURL!
-    )[0]
+      const eventFromURL = context.eventosFromPantry.filter(
+        evento =>
+          evento.eventTitle.toLocaleLowerCase().replace(' ', '-') ===
+          eventNameFromURL!
+      )[0]
 
-    if (eventFromURL) {
-      handleOpenModal(
-        eventFromURL.eventTitle,
-        eventFromURL.eventCover,
-        eventFromURL.eventHTML
+      if (eventFromURL) {
+        handleOpenModal(
+          eventFromURL.eventTitle,
+          eventFromURL.eventCover,
+          eventFromURL.eventHTML
+        )
+        setOpenModal(true)
+      }
+    }, [context])
+
+    const markdownConverter = new showdown.Converter()
+
+    function handleOpenModal(
+      eventTitle: string,
+      eventCover: string,
+      eventHTML: string
+    ) {
+      setModalEventTitle(eventTitle)
+      setModalEventCover(eventCover)
+      setModalEventHTML(
+        eventHTML
+          .split('\n')
+          .map((line: string) => markdownConverter.makeHtml(line))
+          .join('\n')
       )
       setOpenModal(true)
     }
-  }, [context])
 
-  const markdownConverter = new showdown.Converter()
-
-  function handleOpenModal(
-    eventTitle: string,
-    eventCover: string,
-    eventHTML: string
-  ) {
-    setModalEventTitle(eventTitle)
-    setModalEventCover(eventCover)
-    setModalEventHTML(
-      eventHTML
-        .split('\n')
-        .map((line: string) => markdownConverter.makeHtml(line))
-        .join('\n')
-    )
-    setOpenModal(true)
-  }
-
-  function handleCloseModal() {
-    setOpenModal(false)
-    if (eventNameFromURL.split('?').length > 0) {
-      setEventNameFromURL('')
-      window.history.pushState({}, document.title, '/')
+    function handleCloseModal() {
+      setOpenModal(false)
+      if (eventNameFromURL.split('?').length > 0) {
+        setEventNameFromURL('')
+        window.history.pushState({}, document.title, '/')
+      }
     }
-  }
 
-  // local component
-  function Evento({
-    eventTitle,
-    eventDate,
-    eventCover,
-    eventHTML,
-  }: EventoProps) {
-    return (
-      <Fragment>
-        <Modal
-          id={`${modalEventTitle.replace(' ', '-').toLocaleLowerCase()}`}
-          open={openModal}
-          onClose={handleCloseModal}
-          aria-labelledby='modal-modal-title'
-          aria-describedby='modal-modal-description'
-          sx={{
-            width: 'calc(min(90%,600px))',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            margin: '0 auto',
-          }}
-        >
-          <>
-            <Typography
-              variant='h4'
-              color={'white'}
-              onClick={handleCloseModal}
-              align='right'
-              style={{ cursor: 'pointer' }}
-            >
-              X
-            </Typography>
-            <Box
-              style={{
-                overflow: 'scroll',
-                backgroundColor: 'black',
-                paddingBottom: '20px',
-              }}
-            >
-              <img
-                src={modalEventCover}
-                style={{ width: '100%' }}
-                alt='event cover'
-              />
-
+    // local component
+    function Evento({
+      eventTitle,
+      eventDate,
+      eventCover,
+      eventHTML,
+    }: EventoProps) {
+      return (
+        <Fragment>
+          <Modal
+            id={`${modalEventTitle.replace(' ', '-').toLocaleLowerCase()}`}
+            open={openModal}
+            onClose={handleCloseModal}
+            aria-labelledby='modal-modal-title'
+            aria-describedby='modal-modal-description'
+            sx={{
+              width: 'calc(min(90%,600px))',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              margin: '0 auto',
+            }}
+          >
+            <>
+              <Typography
+                variant='h4'
+                color={'white'}
+                onClick={handleCloseModal}
+                align='right'
+                style={{ cursor: 'pointer' }}
+              >
+                X
+              </Typography>
               <Box
                 style={{
-                  padding: '20px',
+                  overflow: 'scroll',
+                  backgroundColor: 'black',
+                  paddingBottom: '20px',
                 }}
               >
-                <Typography
-                  id='modal-modal-title'
-                  variant='h6'
-                  component='h2'
-                  color={'white'}
-                >
-                  {modalEventTitle}
-                </Typography>
-                <Typography id='modal-modal-description' color={'white'}>
-                  {modalEventDate}
-                </Typography>
-                <div
-                  className='beautifulWhiteText'
-                  dangerouslySetInnerHTML={{
-                    __html: modalEventHTML,
-                  }}
+                <img
+                  src={modalEventCover}
+                  style={{ width: '100%' }}
+                  alt='event cover'
                 />
-              </Box>
-            </Box>
-          </>
-        </Modal>
-        <p style={{ fontWeight: 'bold', alignSelf: 'start', fontSize: '16px' }}>
-          Eventos
-        </p>
 
-        <div
-          className={styles.container}
-          onClick={() => handleOpenModal(eventTitle, eventCover, eventHTML)}
-        >
-          <div style={{ width: '150px' }}>
-            <img
-              src={eventCover}
-              style={{ width: 'auto', height: '150px' }}
-              alt='event cover'
-            />
+                <Box
+                  style={{
+                    padding: '20px',
+                  }}
+                >
+                  <Typography
+                    id='modal-modal-title'
+                    variant='h6'
+                    component='h2'
+                    color={'white'}
+                  >
+                    {modalEventTitle}
+                  </Typography>
+                  <Typography id='modal-modal-description' color={'white'}>
+                    {modalEventDate}
+                  </Typography>
+                  <div
+                    className='beautifulWhiteText'
+                    dangerouslySetInnerHTML={{
+                      __html: modalEventHTML,
+                    }}
+                  />
+                </Box>
+              </Box>
+            </>
+          </Modal>
+          <p
+            style={{ fontWeight: 'bold', alignSelf: 'start', fontSize: '16px' }}
+          >
+            Eventos
+          </p>
+
+          <div
+            className={styles.container}
+            onClick={() => handleOpenModal(eventTitle, eventCover, eventHTML)}
+          >
+            <div style={{ width: '150px' }}>
+              <img
+                src={eventCover}
+                style={{ width: 'auto', height: '150px' }}
+                alt='event cover'
+              />
+            </div>
+            <div className={styles.descriptionContainer}>
+              <h2 className={styles.title}>{eventTitle}</h2>
+              <h2 className={styles.date}>{eventDate}</h2>
+              <p style={{ fontWeight: 'bold' }}>Saiba mais</p>
+            </div>
           </div>
-          <div className={styles.descriptionContainer}>
-            <h2 className={styles.title}>{eventTitle}</h2>
-            <h2 className={styles.date}>{eventDate}</h2>
-            <p style={{ fontWeight: 'bold' }}>Saiba mais</p>
-          </div>
-        </div>
-      </Fragment>
+        </Fragment>
+      )
+    }
+
+    return (
+      <MyCarousel
+        items={context.eventosFromPantry.map((evento: any, i: any) => (
+          <Evento key={i} {...evento} eventCover={evento.eventCover} />
+        ))}
+      />
     )
   }
-
-  return (
-    <MyCarousel
-      items={context.eventosFromPantry.map((evento: any, i: any) => (
-        <Evento key={i} {...evento} eventCover={evento.eventCover} />
-      ))}
-    />
-  )
+  return <></>
 }
